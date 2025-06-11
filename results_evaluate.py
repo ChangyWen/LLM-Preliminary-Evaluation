d55e12f323f50fd9918cb231180f19d73558b3e4
ChangyWen <ddcwen@gmail.com>
2025-06-11 20:45:31 +0800

    chore: results

--- results_evaluate.py
+++ results_evaluate.py
@@ -2,24 +2,35 @@ import os
 import sys
 import re
 
-log_dir = sys.argv[1]
+log_dirs = [
+    "grok-3-mini/thinking_high/",
+    "grok-3-mini/thinking_low/",
+    "Qwen3-14B/thinking_False/",
+    "Qwen3-14B/thinking_True/",
+    "DeepSeek-R1-0528/max_tokens_32000/",
+]
 
-result = []
+for log_dir in log_dirs:
+    for group in ["group_1-1", "group_1-2", "group_2-1", "group_2-2"]:
+        log_dir_group = log_dir + group + "/"
+        result = []
 
-# read the last 10 lines of the log file
-log_files = os.listdir("./logs/" + log_dir)
-log_files.sort(key=lambda x: int(x.split('_')[1][:-4]))
-for log_file in log_files:
-    with open(os.path.join("./logs/" + log_dir, log_file), 'r') as f:
-        log_lines = f.readlines()[-15:]
-        all_lines_str = "".join(log_lines)
-        isYes = bool(re.search(r'\bYES\b', all_lines_str.upper()))
-        isNo = bool(re.search(r'\bNO\b', all_lines_str.upper()))
-        if isYes and not isNo:
-            result.append(1)
-        elif isNo and not isYes:
-            result.append(0)
-        else:
-            result.append(-1)
+        # read the last 10 lines of the log file
+        log_files = os.listdir("./logs/" + log_dir_group)
+        log_files.sort(key=lambda x: int(x.split('_')[1][:-4]))
+        for log_file in log_files:
+            with open(os.path.join("./logs/" + log_dir_group, log_file), 'r') as f:
+                log_lines = f.readlines()[-15:]
+                all_lines_str = "".join(log_lines)
+                isYes = bool(re.search(r'\bYES\b', all_lines_str.upper()))
+                isNo = bool(re.search(r'\bNO\b', all_lines_str.upper()))
+                if isYes and not isNo:
+                    result.append(1)
+                elif isNo and not isYes:
+                    result.append(0)
+                else:
+                    result.append(-1)
 
-print(result)
+        print(log_dir_group)
+        print(result)
+        print("\n\n")
